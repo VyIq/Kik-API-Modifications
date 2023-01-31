176cf1a5c7413ff99b0c480c7723ce5871f1a1b6
Milk <105567237+VyIq@users.noreply.github.com>
2023-01-30 19:36:06 -0500

    Update client.py
    Moved to main

--- client.py
+++ client.py
@@ -404,6 +404,15 @@ class KikClient:
         log.info("[+] Trying to join the group '{}' with JID {}".format(group_hashtag, group_jid))
         return self._send_xmpp_element(roster.GroupJoinRequest(group_hashtag, join_token, group_jid))
 
+    def join_group_with_invite_link(self, group_jid, invite_link):
+        """
+        Join into a specific group, using an invite link.
+        :param group_jid: The JID of the same group
+        :param invite_link: An invite code (Looks like 'kik.me/g/{random characters}')
+        """
+        log.info("[+] Joining the group '{}' with JID {}".format(invite_link, group_jid))
+        return self._send_xmpp_element(roster.JoinByInviteLinkRequest(group_jid, invite_link))
+
     def leave_group(self, group_jid):
         """
         Leaves a specific group
