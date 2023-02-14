bf493e1db11a30eeb14967602c0eb0c0297ba0e2
Milk <105567237+VyIq@users.noreply.github.com>
2023-02-14 12:45:51 -0500

    Add files via upload

---
+++ Untested-and-Unfinished-Code/email_uniqueness_sign_up.py
@@ -0,0 +1,113 @@
+from bs4 import BeautifulSoup
+from kik_unofficial.datatypes.xmpp.base_elements import XMPPElement, XMPPResponse
+from kik_unofficial.device_configuration import device_id, android_id, kik_version_info
+from kik_unofficial.utilities.cryptographic_utilities import CryptographicUtils
+
+captcha_element = '<challenge><response>{}</response></challenge>'
+
+
+class RegisterRequest(XMPPElement):
+    """
+    Represents a Kik sign up request
+    """
+
+    def __init__(self, email, username, password, first_name, last_name, birthday="1974-11-20", captcha_result=None, device_id_override=None,
+                 android_id_override=None):
+        super().__init__()
+        self.email = email
+        self.username = username
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
+        self.birthday = birthday
+        self.captcha_result = captcha_result
+        self.device_id_override = device_id_override
+        self.android_id_override = android_id_override
+
+    def serialize(self):
+        passkey_e = CryptographicUtils.key_from_password(self.email, self.password)
+        passkey_u = CryptographicUtils.key_from_password(self.username, self.password)
+        captcha = captcha_element.format(self.captcha_result) if self.captcha_result else ''
+        data = ('<iq type="set" id="{}">'
+                '<query xmlns="jabber:iq:register">'
+                '<email>{}</email>'
+                '<passkey-e>{}</passkey-e>'
+                '<passkey-u>{}</passkey-u>'
+                '<device-id>{}</device-id>'
+                '<username>{}</username>'
+                '<first>{}</first>'
+                '<last>{}</last>'
+                '<birthday>{}</birthday>'
+                '{}'
+                '<version>{}</version>'
+                '<device-type>android</device-type>'
+                '<model>Nexus 7</model>'
+                '<android-sdk>25</android-sdk>'
+                '<registrations-since-install>1</registrations-since-install>'
+                '<install-date>unknown</install-date>'
+                '<logins-since-install>0</logins-since-install>'
+                '<prefix>CAN</prefix>'
+                '<lang>en_US</lang>'
+                '<brand>google</brand>'
+                '<android-id>{}</android-id>'
+                '</query>'
+                '</iq>').format(self.message_id, self.email, passkey_e, passkey_u, self.device_id_override if self.device_id_override else device_id,
+                                self.username, self.first_name, self.last_name, self.birthday, captcha, kik_version_info["kik_version"],
+                                self.android_id_override if self.android_id_override else android_id)
+
+        return data.encode()
+
+
+class RegisterResponse(XMPPResponse):
+    """
+    Represents a response for a Kik sign up request.
+    """
+    def __init__(self, data: BeautifulSoup):
+        super().__init__(data)
+        self.kik_node = data.query.node.text
+
+
+class CheckUsernameUniquenessRequest(XMPPElement):
+    def __init__(self, username):
+        super().__init__()
+        self.username = username
+
+    def serialize(self) -> bytes:
+        data = ('<iq type="get" id="{}">'
+                '<query xmlns="kik:iq:check-unique">'
+                '<username>{}</username>'
+                '</query>'
+                '</iq>').format(self.message_id, self.username)
+
+        return data.encode()
+
+
+class UsernameUniquenessResponse(XMPPResponse):
+    def __init__(self, data: BeautifulSoup):
+        super().__init__(data)
+        username_element = data.find('username')
+        self.unique = True if username_element['is-unique'] == "true" else False
+        self.username = username_element.text
+
+
+class CheckEmailUniquenessRequest(XMPPElement):
+    def __init__(self, email):
+        super().__init__()
+        self.email = email
+
+    def serialize(self) -> bytes:
+        data = ('<iq type="get" id="{}">'
+                '<query xmlns="kik:iq:check-unique">'
+                '<email>{}</email>
+                '</query>'
+                '</iq>').format(self.message_id, self.email)
+
+        return data.encode()
+
+
+class EmailUniquenessResponse(XMPPResponse):
+    def __init__(self, data: BeautifulSoup):
+        super()__init__(data)
+        email_element = data.find('email')
+        self.unique = True if email_element['is-unique'] == "true" else False
+        self.email = email_element.text
---
+++ Untested-and-Unfinished-Code/email_uniqueness_xmlns_handlers.py
@@ -0,0 +1,212 @@
+import logging
+
+from bs4 import BeautifulSoup
+
+from kik_unofficial.callbacks import KikClientCallback
+from kik_unofficial.datatypes.xmpp.account import GetMyProfileResponse
+from kik_unofficial.datatypes.xmpp.chatting import IncomingMessageDeliveredEvent, IncomingMessageReadEvent, IncomingChatMessage, \
+    IncomingGroupChatMessage, IncomingFriendAttribution, IncomingGroupStatus, IncomingIsTypingEvent, IncomingGroupIsTypingEvent, \
+    IncomingStatusResponse, IncomingGroupSticker, IncomingGroupSysmsg, IncomingImageMessage, IncomingGroupReceiptsEvent, IncomingGifMessage, \
+    IncomingVideoMessage, IncomingCardMessage
+from kik_unofficial.datatypes.xmpp.errors import SignUpError, LoginError
+from kik_unofficial.datatypes.xmpp.login import LoginResponse
+from kik_unofficial.datatypes.xmpp.roster import FetchRosterResponse, PeersInfoResponse, GroupSearchResponse
+from kik_unofficial.datatypes.xmpp.sign_up import RegisterResponse, UsernameUniquenessResponse, EmailUniquenessResponse
+from kik_unofficial.datatypes.xmpp.xiphias import UsersResponse, UsersByAliasResponse
+from kik_unofficial.datatypes.xmpp.history import HistoryResponse
+
+
+log = logging.getLogger('kik_unofficial')
+
+
+class XmppHandler:
+    def __init__(self, callback: KikClientCallback, client):
+        self.callback = callback
+        self.client = client
+
+    def handle(self, data: BeautifulSoup):
+        raise NotImplementedError
+
+
+class XMPPMessageHandler(XmppHandler):
+    def handle(self, data: BeautifulSoup):
+        if data['type'] == 'chat':
+
+            #
+            # We received some sort of a chat message.
+            #
+
+            if data.body and data.body.text:
+                # regular text message
+                self.callback.on_chat_message_received(IncomingChatMessage(data))
+            elif data.find('friend-attribution'):
+                # friend attribution
+                self.callback.on_friend_attribution(IncomingFriendAttribution(data))
+            elif data.find('status'):
+                # status
+                self.callback.on_status_message_received(IncomingStatusResponse(data))
+            elif data.find('xiphias-mobileremote-call'):
+                # usually SafetyNet-related (?)
+                mobile_remote_call = data.find('xiphias-mobileremote-call')
+                log.warning("[!] Received mobile-remote-call with method '{}' of service '{}'".format(
+                    mobile_remote_call['method'], mobile_remote_call['service']))
+            elif data.find('images'):
+                # images
+                self.callback.on_image_received(IncomingImageMessage(data))
+            else:
+                # what else? GIFs?
+                log.debug("[-] Received unknown chat message. contents: {}".format(str(data)))
+
+        elif data['type'] == 'receipt':
+            #
+            # We received some sort of a receipt.
+            #
+
+            if data.g:
+                self.callback.on_group_receipts_received(IncomingGroupReceiptsEvent(data))
+            elif data.receipt['type'] == 'delivered':
+                self.callback.on_message_delivered(IncomingMessageDeliveredEvent(data))
+            else:
+                self.callback.on_message_read(IncomingMessageReadEvent(data))
+
+        elif data['type'] == 'is-typing':
+            #
+            # Some user started to type or stopped to type
+            #
+
+            self.callback.on_is_typing_event_received(IncomingIsTypingEvent(data))
+
+
+        elif data['type'] == 'groupchat':
+            #
+            # We received some sort of a group chat message.
+            #
+
+            if data.body:
+                self.callback.on_group_message_received(IncomingGroupChatMessage(data))
+            elif data.find('is-typing'):
+                self.callback.on_group_is_typing_event_received(IncomingGroupIsTypingEvent(data))
+            elif data.find('status'):
+                self.callback.on_group_status_received(IncomingGroupStatus(data))
+            elif data.find('sysmsg'):
+                self.callback.on_group_sysmsg_received(IncomingGroupSysmsg(data))
+            else:
+                log.debug("[-] Received unknown groupchat message. contents: {}".format(str(data)))
+        else:
+            log.debug("[-] Received unknown message type. contents: {}".format(str(data)))
+
+
+class GroupXMPPMessageHandler(XmppHandler):
+    def handle(self, data: BeautifulSoup):
+        if data.body:
+            self.callback.on_group_message_received(IncomingGroupChatMessage(data))
+        elif data.find('is-typing'):
+            self.callback.on_group_is_typing_event_received(IncomingGroupIsTypingEvent(data))
+        elif data.find('status'):
+            self.callback.on_group_status_received(IncomingGroupStatus(data))
+        elif data.find('sysmsg'):
+            self.callback.on_group_sysmsg_received(IncomingGroupSysmsg(data))
+        elif data.content and 'app-id' in data.content.attrs:
+            app_id = data.content['app-id']
+            if app_id == 'com.kik.ext.stickers':
+                self.callback.on_group_sticker(IncomingGroupSticker(data))
+            elif app_id == 'com.kik.ext.gallery':
+                self.callback.on_image_received(IncomingImageMessage(data))
+            elif app_id == 'com.kik.ext.camera':
+                self.callback.on_image_received(IncomingImageMessage(data))
+            elif app_id == 'com.kik.ext.gif':
+                self.callback.on_gif_received(IncomingGifMessage(data))
+            elif app_id == 'com.kik.ext.video-camera':
+                self.callback.on_video_received(IncomingVideoMessage(data))
+            elif app_id == 'com.kik.ext.video-gallery':
+                self.callback.on_video_received(IncomingVideoMessage(data))
+            elif app_id == 'com.kik.cards':
+                self.callback.on_card_received(IncomingCardMessage(data))
+
+        else:
+            log.debug("[-] Received unknown group message. contents: {}".format(str(data)))
+
+
+class HistoryHandler(XmppHandler):
+    def handle(self, data: BeautifulSoup):
+        self.callback.on_message_history_response(HistoryResponse(data))
+
+
+class UserProfileHandler(XmppHandler):
+    def handle(self, data: BeautifulSoup):
+        # this will ignore results for other requests
+        # like email change that also use the kik:iq:user-profile namespace
+        if data.username:
+            self.callback.on_get_my_profile_response(GetMyProfileResponse(data))
+
+
+class CheckUsernameUniqueResponseHandler(XmppHandler):
+    def handle(self, data: BeautifulSoup):
+        self.callback.on_username_uniqueness_received(UsernameUniquenessResponse(data))
+
+
+class CheckEmailUniqueResponseHandler(XmmpHandler):
+    def handle(self, data: BeautifulSoup):
+        self.callback.on_email_uniqueness_received(EmailUniquenessResponse(data))
+
+
+class RegisterOrLoginResponseHandler(XmppHandler):
+    def handle(self, data: BeautifulSoup):
+        message_type = data['type']
+
+        if message_type == "error":
+            if data.find('email'):
+                # sign up
+                sign_up_error = SignUpError(data)
+                log.info("[-] Register error: {}".format(sign_up_error))
+                self.callback.on_register_error(sign_up_error)
+
+            else:
+                login_error = LoginError(data)
+                log.info("[-] Login error: {}".format(login_error))
+                self.callback.on_login_error(login_error)
+
+        elif message_type == "result":
+            if data.find('email'):
+                # login successful
+                response = LoginResponse(data)
+                self.client.username = response.username
+                self.client.kik_node = response.kik_node
+                self.client.kik_email = response.email
+                log.info("[+] Logged in as {}".format(response.username))
+                self.callback.on_login_ended(response)
+                self.client._establish_authenticated_session(response.kik_node)
+            else:
+                # sign up successful
+                response = RegisterResponse(data)
+                log.info("[+] Registered.")
+                self.callback.on_sign_up_ended(response)
+                self.client._establish_authenticated_session(response.kik_node)
+
+
+class RosterResponseHandler(XmppHandler):
+    def handle(self, data: BeautifulSoup):
+        self.callback.on_roster_received(FetchRosterResponse(data))
+
+
+class PeersInfoResponseHandler(XmppHandler):
+    def handle(self, data: BeautifulSoup):
+        peers_info = PeersInfoResponse(data)
+
+        # add this user to the list of known users if it wasn't encountered before
+        for peer_info in peers_info.users:
+            self.client._known_users_information.add(peer_info)
+        self.client._new_user_added_event.set()
+
+        self.callback.on_peer_info_received(peers_info)
+
+
+class XiphiasHandler(XmppHandler):
+    def handle(self, data: BeautifulSoup):
+        method = data.query['method']
+        if method == 'GetUsers':
+            self.callback.on_xiphias_get_users_response(UsersResponse(data))
+        elif method == 'GetUsersByAlias':
+            self.callback.on_xiphias_get_users_response(UsersByAliasResponse(data))
+        else:  # TODO
+            self.callback.on_group_search_response(GroupSearchResponse(data))
